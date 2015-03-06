4b67d4511f065cbb4f7eb94f50fa74c3b23c2348
Hrovje Baic <baic989@gmail.com>
2015-03-06 21:37:27 +0100

    Initial Java commit with Hello World

---
+++ Java/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author hrvoje
+ */
+public class HelloWorld {
+
+//Without tradition, art is a flock of sheep without a shepherd. 
+//Without innovation, it is a corpse.
+
+//Winston Churchill
+    public static void main(String[] args) {
+        System.out.println("Hello World! :)");
+    }
+}
