97e28af7d8ca833fd6f681ba83b92d6c57004fae
Hrvoje Baic <baic989@gmail.com>
2015-03-31 19:35:29 +0200

    In the making.

---
+++ WinForms/TileSlideGame/OOP_NET_DZ1_Hrvoje_Baic/TileSlidingGame/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TileSlidingGame
+{
+    partial class gameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadPictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mountainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bSODToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shuffleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gamePanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.authorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sizeToolStripMenuItem,
+            this.loadPictureToolStripMenuItem,
+            this.shuffleToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(404, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x3ToolStripMenuItem,
+            this.x4ToolStripMenuItem});
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.sizeToolStripMenuItem.Text = "Size";
+            // 
+            // x3ToolStripMenuItem
+            // 
+            this.x3ToolStripMenuItem.Name = "x3ToolStripMenuItem";
+            this.x3ToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.x3ToolStripMenuItem.Text = "3 x 3";
+            this.x3ToolStripMenuItem.Click += new System.EventHandler(this.x3ToolStripMenuItem_Click);
+            // 
+            // x4ToolStripMenuItem
+            // 
+            this.x4ToolStripMenuItem.Name = "x4ToolStripMenuItem";
+            this.x4ToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.x4ToolStripMenuItem.Text = "4 x 4";
+            this.x4ToolStripMenuItem.Click += new System.EventHandler(this.x4ToolStripMenuItem_Click);
+            // 
+            // loadPictureToolStripMenuItem
+            // 
+            this.loadPictureToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dogToolStripMenuItem,
+            this.mountainToolStripMenuItem,
+            this.bSODToolStripMenuItem});
+            this.loadPictureToolStripMenuItem.Name = "loadPictureToolStripMenuItem";
+            this.loadPictureToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.loadPictureToolStripMenuItem.Text = "Load Picture";
+            // 
+            // dogToolStripMenuItem
+            // 
+            this.dogToolStripMenuItem.Name = "dogToolStripMenuItem";
+            this.dogToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.dogToolStripMenuItem.Text = "Dog";
+            this.dogToolStripMenuItem.Click += new System.EventHandler(this.dogToolStripMenuItem_Click);
+            // 
+            // mountainToolStripMenuItem
+            // 
+            this.mountainToolStripMenuItem.Name = "mountainToolStripMenuItem";
+            this.mountainToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.mountainToolStripMenuItem.Text = "Mountain";
+            this.mountainToolStripMenuItem.Click += new System.EventHandler(this.mountainToolStripMenuItem_Click);
+            // 
+            // bSODToolStripMenuItem
+            // 
+            this.bSODToolStripMenuItem.Name = "bSODToolStripMenuItem";
+            this.bSODToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.bSODToolStripMenuItem.Text = "BSOD";
+            this.bSODToolStripMenuItem.Click += new System.EventHandler(this.bSODToolStripMenuItem_Click);
+            // 
+            // shuffleToolStripMenuItem
+            // 
+            this.shuffleToolStripMenuItem.Name = "shuffleToolStripMenuItem";
+            this.shuffleToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.shuffleToolStripMenuItem.Text = "Shuffle";
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gamePanel.Location = new System.Drawing.Point(0, 24);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(404, 358);
+            this.gamePanel.TabIndex = 1;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.authorToolStripMenuItem,
+            this.gameToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // authorToolStripMenuItem
+            // 
+            this.authorToolStripMenuItem.Name = "authorToolStripMenuItem";
+            this.authorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.authorToolStripMenuItem.Text = "Author";
+            this.authorToolStripMenuItem.Click += new System.EventHandler(this.authorToolStripMenuItem_Click);
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gameToolStripMenuItem.Text = "Game";
+            this.gameToolStripMenuItem.Click += new System.EventHandler(this.gameToolStripMenuItem_Click);
+            // 
+            // gameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 382);
+            this.ControlBox = false;
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "gameForm";
+            this.Text = "Tile Sliding Game by Hrvoje";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadPictureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shuffleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mountainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bSODToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel gamePanel;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem authorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+    }
+}
+
---
+++ WinForms/TileSlideGame/OOP_NET_DZ1_Hrvoje_Baic/TileSlidingGame/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TileSlidingGame
+{
+    public partial class gameForm : Form
+    {
+        // Starting image and size, panels for different sizes
+        Image currentImage;
+        Panel[] x3Box;
+        Panel[] x4Box;
+        int size;
+
+        public gameForm()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            // Initialize starting values
+            size = 3;
+            currentImage = gameResources.crazy_dog_pics_part2_52;
+            x3Box = new Panel[9];
+            x4Box = new Panel[16];
+
+            // Initialize new game
+            DrawNewImage(size);
+        }
+
+        // Click on "3x3" will set the new game
+        private void x3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            size = 3;
+            // Clear the previous game
+            gamePanel.Controls.Clear();
+
+            // Draw and slice picture
+            DrawNewImage(size);
+        }
+
+        // Click on "4x4" will set the new game
+        private void x4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            size = 4;
+            // Clear the previous game
+            gamePanel.Controls.Clear();
+
+            // Draw and slice picture
+            DrawNewImage(size);
+
+        }
+
+        // Setting a dog image from the menu
+        private void dogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentImage = gameResources.crazy_dog_pics_part2_52;
+
+            gamePanel.Controls.Clear();
+
+            DrawNewImage(size);
+        }
+
+        // Setting a mountain image from the menu
+        private void mountainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentImage = gameResources.mountain_04;
+
+            gamePanel.Controls.Clear();
+
+            DrawNewImage(size);
+        }
+
+        // Setting a BSOD image from the menu
+        private void bSODToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentImage = gameResources.Windows_9X_BSOD;
+
+            gamePanel.Controls.Clear();
+
+            DrawNewImage(size);
+        }
+
+        // Divide and conquer
+        private void DrawNewImage(int size)
+        {
+            Image resizedImage = resizeImage(currentImage, new Size(gamePanel.Width, gamePanel.Height));
+
+            if (size == 3)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        int index = i * size + j;
+
+                        // We will be inserting panels with cropped images on a parent panel
+                        Panel p = new Panel();
+                        p.Width = gamePanel.Width / size;
+                        p.Height = gamePanel.Height / size;
+                        p.Margin = new Padding(0);
+                        p.Click += p_Click;
+
+                        x3Box[index] = p;
+
+                        // Setting a background image
+                        x3Box[index].BackgroundImage = new Bitmap(gamePanel.Width / size, gamePanel.Height / size);
+                        Graphics g = Graphics.FromImage(x3Box[index].BackgroundImage);
+
+                        // Cropping
+                        g.DrawImage(resizedImage, new Rectangle(0, 0, gamePanel.Width / size, gamePanel.Height/ size), new Rectangle(i * gamePanel.Width / size, j * gamePanel.Height / size, gamePanel.Width / size, gamePanel.Height / size), GraphicsUnit.Pixel);
+                        g.Dispose();
+                    }
+                }
+
+                for (int i = 0; i < 9; i++)
+                {
+                    gamePanel.Controls.Add(x3Box[i]);
+                }
+
+                x3Box[0].BackgroundImage = null;
+            }
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        int index = i * size + j;
+
+                        // We will be inserting panels with cropped images on a parent panel
+                        Panel p = new Panel();
+                        p.Width = gamePanel.Width / size;
+                        p.Height = gamePanel.Height / size;
+                        p.Margin = new Padding(0);
+                        p.Click += p_Click;
+
+                        x4Box[index] = p;
+
+                        // Setting a background image
+                        x4Box[index].BackgroundImage = new Bitmap(gamePanel.Width / size, gamePanel.Height / size);
+                        Graphics g = Graphics.FromImage(x4Box[index].BackgroundImage);
+
+                        // Cropping
+                        g.DrawImage(resizedImage, new Rectangle(0, 0, gamePanel.Width / size, gamePanel.Height / size), new Rectangle(i * gamePanel.Width / size, j * gamePanel.Height / size, gamePanel.Width / size, gamePanel.Height / size), GraphicsUnit.Pixel);
+
+                        g.Dispose();
+                    }
+                }
+
+                for (int i = 0; i < 16; i++)
+                {
+                    gamePanel.Controls.Add(x4Box[i]);
+                }
+
+                x4Box[0].BackgroundImage = null;
+            }
+        }
+
+        // Clicked panel will check if there are any free spaces to move
+        void p_Click(object sender, EventArgs e)
+        {
+            if (size == 3)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private Image resizeImage(Image currentImage, Size size)
+        {
+            return (Image)(new Bitmap(currentImage, size));
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        private void gameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A simple tile sliding game.\n\nClick on the pictures to move them around and reconstruct the original image.\nThere are '3x3' and '4x4' options available.\n\nGood luck!");
+        }
+
+        private void authorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Author:\t Hrvoje Baic \n\nGitHub:\t https://github.com/baic989 \nBlog:\t http://imnotswift.com");
+        }
+    }
+}
